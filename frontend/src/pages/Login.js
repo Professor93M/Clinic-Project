8b21036be7e8f5d9af4121cd4cc40de54b2db0fa
watan <drmody007.d0@gmail.com>
2022-01-01 18:38:23 +0300

    request issue

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,115 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../components/form/Button";
+import Input from "../components/form/Input";
+import Label from "../components/form/Label";
+import { postData } from "../utils/fetchData";
 
 const Login = () => {
+  const initailState = {
+    email: "",
+    password: "",
+  };
+
+  const [user, setUser] = useState(initailState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const res = await postData("register", user);
+    console.log(res);
+  };
+  const { email, password } = user;
+
   return (
     <div>
       <h1>Login</h1>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. In harum sunt eos
-      laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore
-      nam.laboriosam sint? Nisi ab, rem, repellendus earum accusamus temporibus
-      mollitia cum dignissimos officiis architecto odio doloremque ipsa eum!
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum dolor
-      quam in, praesentium ducimus deserunt magnam vel unde ab impedit fugit
-      aperiam culpa, accusamus quod rem repellat consectetur tempore nam.
+      <form action="" className="mx-auto text-[1.5rem]">
+        <div className="mb-4">
+          <Label text="اسم المستخدم" elNme="email" />
+          <Input
+            name="email"
+            type="email"
+            handleChange={handleChange}
+            value={email}
+          />
+        </div>
+        <div className="mb-4">
+          <Label text="البريد الالكتروني" elNme="password" />
+          <Input
+            name="password"
+            type="password"
+            handleChange={handleChange}
+            value={password}
+          />
+        </div>
+        <Button text="تسجيل" className="" handleClick={handleClick} />
+      </form>
     </div>
   );
 };
