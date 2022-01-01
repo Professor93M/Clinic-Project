7cb53abed10699a7ed9ca054fbc56ea55f2d63b7
watan <drmody007.d0@gmail.com>
2022-01-01 23:01:27 +0300

    fix the issue of content

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -19,35 +19,37 @@ const Login = () => {
 
   const handleClick = async (e) => {
     e.preventDefault();
-    const res = await postData("register", user);
+    const res = await postData("login", user);
     console.log(res);
   };
   const { email, password } = user;
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form action="" className="mx-auto text-[1.5rem]">
-        <div className="mb-4">
-          <Label text="اسم المستخدم" elNme="email" />
-          <Input
-            name="email"
-            type="email"
-            handleChange={handleChange}
-            value={email}
-          />
-        </div>
-        <div className="mb-4">
-          <Label text="البريد الالكتروني" elNme="password" />
-          <Input
-            name="password"
-            type="password"
-            handleChange={handleChange}
-            value={password}
-          />
-        </div>
-        <Button text="تسجيل" className="" handleClick={handleClick} />
-      </form>
+    <div className=" w-full flex-col flex items-center h-[80vh] bg-slate-900">
+      <div className="max-w-xl  text-center font-semibold text-5xl flex flex-col bg-slate-500">
+        <h1>Login</h1>
+        <form action="" className="mx-auto text-[1.5rem]">
+          <div className="mb-4">
+            <Label text="البريد الالكتروني" elNme="email" />
+            <Input
+              name="email"
+              type="email"
+              handleChange={handleChange}
+              value={email}
+            />
+          </div>
+          <div className="mb-4">
+            <Label text="الرمز السري" elNme="password" />
+            <Input
+              name="password"
+              type="password"
+              handleChange={handleChange}
+              value={password}
+            />
+          </div>
+          <Button text="تسجيل" className="" handleClick={handleClick} />
+        </form>
+      </div>
     </div>
   );
 };
